036b1fa46bd5b81401cfb1e71f2d3348c491e626
ertucode <ertugrulsirtt@gmail.com>
2023-10-22 01:39:56 +0300

    angular imports

--- lua/ertu/ng-comp.lua
+++ lua/ertu/ng-comp.lua
@@ -61,3 +61,99 @@ export class %sComponent {
     ]])
 	html_file:close()
 end)
+
+local query_tail = [[
+    (decorator
+        (call_expression
+            (arguments
+                (object
+                    (pair
+                        key: (property_identifier) @imports (#eq? @imports "imports")
+                        value: (array) @value
+                    )
+                )
+            )
+        )
+    )
+]]
+local query = vim.treesitter.query.parse(
+	"typescript",
+
+	string.format(
+		[[
+[
+(
+  class_declaration
+    %s
+)
+(
+  export_statement
+    %s
+)
+]
+    ]],
+		query_tail,
+		query_tail
+	)
+)
+
+local get_root = function(bufnr)
+	local parser = vim.treesitter.get_parser(bufnr, "typescript", {})
+	local tree = parser:parse()[1]
+	return tree:root()
+end
+
+vim.keymap.set("n", "<leader>ngi", function()
+	local root = get_root(0)
+
+	local found = {}
+
+	for id, node in query:iter_captures(root, 0, 0, -1) do
+		if query.captures[id] == "value" then
+			table.insert(found, node)
+		end
+	end
+
+	local use = function(node)
+		local row1, col1, row2, col2 = node:range() -- range of the capture
+		local on_same_line = row1 == row2
+
+		if on_same_line then
+			vim.api.nvim_win_set_cursor(0, { row2 + 1, col2 - 1 })
+			vim.cmd("startinsert")
+			vim.api.nvim_input(", ")
+		else
+			local line = vim.api.nvim_buf_get_lines(0, row2 - 1, row2 - 0, false)[1]
+			local fill = string.sub(line, 0, string.find(line, "[^%s]", 0) - 1) .. " "
+			vim.api.nvim_buf_set_lines(0, row2, row2, false, { fill })
+
+			vim.api.nvim_win_set_cursor(0, { row2 + 1, #fill or 0 })
+			vim.cmd("startinsert")
+		end
+	end
+
+	if #found == 0 then
+		return
+	end
+
+	if #found == 1 then
+		use(found[1])
+		return
+	end
+
+	local closest = nil
+	local min = 1000000
+	local cur_pos = vim.api.nvim_win_get_cursor(0)[1]
+	for _, node in ipairs(found) do
+		local row1, _, _, _ = node:range() -- range of the capture
+		local dist = math.abs(row1 - cur_pos)
+		if dist < min then
+			min = dist
+			closest = node
+		end
+	end
+
+	if not closest == nil then
+		use(closest)
+	end
+end)
--- lua/ertu/packer.lua
+++ lua/ertu/packer.lua
@@ -19,6 +19,8 @@ return require("packer").startup(function(use)
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 	use("nvim-treesitter/nvim-treesitter-context")
 	use("nvim-treesitter/nvim-treesitter-textobjects")
+	use("nvim-treesitter/playground")
+
 	use("theprimeagen/harpoon")
 	use("mbbill/undotree")
 	use("tpope/vim-fugitive")
