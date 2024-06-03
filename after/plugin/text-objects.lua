526490858b93584d31b7cedb27825191cc85ad03
ertucode <ertugrulsirtt@gmail.com>
2024-06-03 22:04:39 +0300

    better keymap

--- after/plugin/text-objects.lua
+++ after/plugin/text-objects.lua
@@ -14,8 +14,8 @@ require("nvim-treesitter.configs").setup({
 				["ic"] = "@class.inner",
 				["ak"] = "@call.outer",
 				["ik"] = "@call.inner",
-				["mr"] = "@assignment.rhs",
-				["ml"] = "@assignment.lhs",
+				["ir"] = "@assignment.rhs",
+				["ie"] = "@assignment.lhs",
 				-- You can also use captures from other query groups like `locals.scm`
 				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 			},
