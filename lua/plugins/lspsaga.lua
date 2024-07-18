8edf2c2d27d095e78c46dd36e3645f0bc7ef3823
ertucode <ertugrulsirtt@gmail.com>
2024-07-18 22:27:09 +0300

    klasdf

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -5,11 +5,20 @@ return {
 			symbol_in_winbar = {
 				enable = false,
 			},
+			lightbulb = {
+				enable = false,
+			},
+			implement = {
+				enable = false,
+			},
+			beacon = {
+				enable = false,
+			},
 		})
 	end,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter", -- optional
 		"nvim-tree/nvim-web-devicons", -- optional
 	},
-	enabled = false,
+	enabled = true,
 }
