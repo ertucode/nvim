005e7d60b63ffc3a85dc468d924735b769377f0d
ertucode <ertugrulsirtt@gmail.com>
2024-11-30 21:42:45 +0300

    stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,4 +9,11 @@ return {
 		event = "BufRead",
 		dependencies = { "kana/vim-textobj-user" },
 	},
+	{
+		-- Enable when the trial ends. Clear device id and stuff. ~/Library/Preferences/com.apple.java.util.prefs.plist
+		-- Or try removing the file
+		-- https://gist.github.com/XGilmar/55f6727a40f78979a6a7f8d5885e3bdd?permalink_comment_id=5057636
+		"darfink/vim-plist",
+		enabled = false,
+	},
 }
