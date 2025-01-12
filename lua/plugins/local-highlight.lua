21f9d09a5c702ab88068dc5c8f3616f339957851
ertucode <ertugrulsirtt@gmail.com>
2025-01-12 22:12:19 +0300

    a

--- lua/plugins/local-highlight.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tzachar/local-highlight.nvim",
-	config = function()
-		require("local-highlight").setup({
-			cw_hlgroup = "LocalHighlight",
-			highlight_single_match = false,
-		})
-	end,
-}
