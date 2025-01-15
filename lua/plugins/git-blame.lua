4a6ff1495b13863e66997310840d217198bbad10
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-01-15 16:33:52 +0300

    better blame

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -1,5 +1,12 @@
 return {
 	"f-person/git-blame.nvim",
-	-- load the plugin at startup
-	cmd = "GitBlameToggle",
+	-- load the plugin at startup, did not work
+	-- cmd = "GitBlameToggle",
+	event = "VeryLazy",
+	config = function()
+		require("gitblame").setup({
+			enabled = false,
+		})
+		vim.keymap.set("n", "<leader>bt", ":GitBlameToggle<CR>")
+	end,
 }
