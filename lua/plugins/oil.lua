53636656b63313d8ad416f1969608ded591cb694
ertucode <ertugrulsirtt@gmail.com>
2025-01-31 01:26:00 +0300

    laksjdf

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,25 +1,54 @@
+-- return {
+-- 	"stevearc/oil.nvim",
+-- 	-- tag = "v2.2.0",
+-- 	config = function()
+-- 		local status, oil = pcall(require, "oil")
+--
+-- 		if not status then
+-- 			return
+-- 		end
+--
+-- 		oil.setup({
+-- 			skip_confirm_for_simple_edits = true,
+-- 			-- lsp_file_methods = {
+-- 			-- 	enabled = true,
+-- 			-- },
+-- 			keymaps = {
+-- 				["<C-p>"] = false,
+-- 				["<C-s>"] = false,
+-- 			},
+-- 			watch_for_changes = true,
+-- 		})
+--
+-- 		vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+-- 	end,
+-- }
+
 return {
-	"stevearc/oil.nvim",
-	-- tag = "v2.2.0",
-	config = function()
-		local status, oil = pcall(require, "oil")
+  'echasnovski/mini.files', version = false,
+  config = function()
+    local mini = require('mini.files')
+    mini.setup({
+      mappings = {
+        go_in_plus  = '<CR>',
+        go_in       = '<C-l>',
+        go_out      = '<C-h>',
+      },
+    })
 
-		if not status then
-			return
-		end
+    vim.keymap.set("n", "-", function ()
+      -- mini.open() 
+      mini.open(vim.api.nvim_buf_get_name(0)) 
+    end, { desc = "Open parent directory" })
 
-		oil.setup({
-			skip_confirm_for_simple_edits = true,
-			-- lsp_file_methods = {
-			-- 	enabled = true,
-			-- },
-			keymaps = {
-				["<C-p>"] = false,
-				["<C-s>"] = false,
-			},
-			watch_for_changes = true,
-		})
-
-		vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-	end,
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TelescopeFindPre",
+      callback = function()
+        local ok, mini_files = pcall(require, "mini.files")
+        if ok then
+          mini_files.close()
+        end
+      end
+    })
+  end,
 }
