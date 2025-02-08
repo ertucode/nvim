6df44c20302c80d5b92c43755e4ab09396304dc5
ertucode <ertugrulsirtt@gmail.com>
2025-02-09 00:48:45 +0300

    mini oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,54 +1,56 @@
--- return {
--- 	"stevearc/oil.nvim",
--- 	-- tag = "v2.2.0",
--- 	config = function()
--- 		local status, oil = pcall(require, "oil")
---
--- 		if not status then
--- 			return
--- 		end
---
--- 		oil.setup({
--- 			skip_confirm_for_simple_edits = true,
--- 			-- lsp_file_methods = {
--- 			-- 	enabled = true,
--- 			-- },
--- 			keymaps = {
--- 				["<C-p>"] = false,
--- 				["<C-s>"] = false,
--- 			},
--- 			watch_for_changes = true,
--- 		})
---
--- 		vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
--- 	end,
--- }
-
 return {
-  'echasnovski/mini.files', version = false,
-  config = function()
-    local mini = require('mini.files')
-    mini.setup({
-      mappings = {
-        go_in_plus  = '<CR>',
-        go_in       = '<C-l>',
-        go_out      = '<C-h>',
-      },
-    })
+	"stevearc/oil.nvim",
+  lazy = false,
+  priority = 100000,
+	-- tag = "v2.2.0",
+	config = function()
+		local status, oil = pcall(require, "oil")
 
-    vim.keymap.set("n", "-", function ()
-      -- mini.open() 
-      mini.open(vim.api.nvim_buf_get_name(0)) 
-    end, { desc = "Open parent directory" })
+		if not status then
+			return
+		end
 
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "TelescopeFindPre",
-      callback = function()
-        local ok, mini_files = pcall(require, "mini.files")
-        if ok then
-          mini_files.close()
-        end
-      end
-    })
-  end,
+		oil.setup({
+			skip_confirm_for_simple_edits = true,
+			-- lsp_file_methods = {
+			-- 	enabled = true,
+			-- },
+			keymaps = {
+				["<C-p>"] = false,
+				["<C-s>"] = false,
+			},
+			watch_for_changes = true,
+		})
+
+		vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+	end,
 }
+
+-- return {
+--   'echasnovski/mini.files', version = false,
+--   config = function()
+--     local mini = require('mini.files')
+--     mini.setup({
+--       mappings = {
+--         go_in_plus  = '<CR>',
+--         go_in       = '<C-l>',
+--         go_out      = '<C-h>',
+--       },
+--     })
+--
+--     vim.keymap.set("n", "-", function ()
+--       -- mini.open() 
+--       mini.open(vim.api.nvim_buf_get_name(0)) 
+--     end, { desc = "Open parent directory" })
+--
+--     vim.api.nvim_create_autocmd("User", {
+--       pattern = "TelescopeFindPre",
+--       callback = function()
+--         local ok, mini_files = pcall(require, "mini.files")
+--         if ok then
+--           mini_files.close()
+--         end
+--       end
+--     })
+--   end,
+-- }
