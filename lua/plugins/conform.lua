74075b8cde531864c63cce1c8452ebb43ba4cd01
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-03-03 09:17:57 +0300

    conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,59 @@
+local function should_format_on_save()
+	local cwd = vim.uv.cwd()
+	if cwd == nil then
+		return
+	end
+
+	local path = "react-native/ortak"
+
+	return string.find(cwd, path)
+end
+
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
+		local format_on_save = should_format_on_save()
+				and {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				}
+			or nil
+
+		conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        angular = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        svelte = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
+				liquid = { "prettierd" },
+				lua = { "stylua" },
+				-- python = { "isort", "black" },
+			},
+			format_on_save = format_on_save,
+		})
+	end,
+	keys = {
+		{
+			"<leader>mp",
+			function()
+				require("conform").format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end,
+			desc = "Format file or range (in visual mode)",
+			mode = { "n", "v" },
+		},
+	},
+}
