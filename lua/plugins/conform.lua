130ea0a51fd0b57cf8c54351f4ca66c425ce1c2e
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-03-24 22:53:20 +0300

    fix format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,17 @@
 local function should_format_on_save()
 	local cwd = vim.uv.cwd()
 	if cwd == nil then
-		return
+		return false
 	end
 
 	local path = "react-native/ortak"
 
-	return string.find(cwd, path)
+	return string.find(cwd, path) == nil
 end
 
 return {
 	"stevearc/conform.nvim",
+  event = "VeryLazy",
 	config = function()
 		local conform = require("conform")
 		local format_on_save = should_format_on_save()
@@ -21,6 +22,14 @@ return {
 				}
 			or nil
 
+    vim.keymap.set({"n", "v"}, "<leader>mp", function()
+      require("conform").format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)", })
+
 		conform.setup({
       formatters_by_ft = {
         javascript = { "prettierd" },
@@ -28,10 +37,10 @@ return {
         angular = { "prettierd" },
         javascriptreact = { "prettierd" },
         typescriptreact = { "prettierd" },
-        svelte = { "prettierd" },
-        css = { "prettierd" },
-        html = { "prettierd" },
-        json = { "prettierd" },
+				svelte = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
 				yaml = { "prettierd" },
 				markdown = { "prettierd" },
 				graphql = { "prettierd" },
@@ -42,18 +51,4 @@ return {
 			format_on_save = format_on_save,
 		})
 	end,
-	keys = {
-		{
-			"<leader>mp",
-			function()
-				require("conform").format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				})
-			end,
-			desc = "Format file or range (in visual mode)",
-			mode = { "n", "v" },
-		},
-	},
 }
