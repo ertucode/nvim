4b613372b5af78b5e80e6f98c2ba96dad924ce9b
ertucode <ertugrulsirtt@gmail.com>
2025-04-05 15:16:41 +0300

    lsp gd

--- lua/ertu/utils/lsp_definition.lua
+++ lua/ertu/utils/lsp_definition.lua
@@ -16,23 +16,54 @@ local function filter(arr, fn)
 	return filtered
 end
 
-local function gdUri(value)
+local function is_typescript_client(client)
+	return client.name == "tsserver" or client.name == "vtsls" or client.name == "ts_ls"
+end
+
+local function result_item_uri(value)
 	return value.uri or value.targetUri
 end
 
 local function filterReactDTS(value)
-	local uri = gdUri(value)
+	local uri = result_item_uri(value)
 	return string.match(uri, "react/index.d.ts") == nil
 end
 
 local function everyOneIsSameLine(values)
-	local uri = gdUri(values[1])
+	local uri = result_item_uri(values[1])
+	local line = values[1].targetRange.start.line
+
 	for _, value in pairs(values) do
-		if gdUri(value) ~= uri then
-			return nil
+		if result_item_uri(value) ~= uri then
+			return false
+		end
+		if value.targetRange.start.line ~= line then
+			return false
 		end
 	end
-	return values[1]
+	return true
+end
+
+local function origin_and_target_same(result_item)
+	local target = result_item.targetSelectionRange -- targetRange?
+	local origin = result_item.originSelectionRange
+	local current_file_uri = vim.uri_from_bufnr(0)
+	if current_file_uri ~= result_item_uri(result_item) then
+		return false
+	end
+	return target["end"]["character"] == origin["end"].character
+		and target["end"]["line"] == origin["end"].line
+		and target["start"]["character"] == origin["start"].character
+		and target["start"]["line"] == origin["start"].line
+end
+
+local function on_response_or_references(error, result_item, client, on_response)
+	if origin_and_target_same(result_item) then
+		require("telescope.builtin").lsp_references()
+		return
+	end
+
+	on_response(error, result_item, client)
 end
 
 --- @param client vim.lsp.Client
@@ -41,43 +72,25 @@ local function custom_on_response(error, result, client, on_response)
 		return on_response(error, result, client)
 	end
 	if not vim.islist(result) or #result == 1 then
-		local first = result[1]
-		local target = first.targetSelectionRange -- targetRange?
-		local origin = first.originSelectionRange
-		if
-			target["end"]["character"] == origin["end"].character
-			and target["end"]["line"] == origin["end"].line
-			and target["start"]["character"] == origin["start"].character
-			and target["start"]["line"] == origin["start"].line
-		then
-			require("telescope.builtin").lsp_references()
-			return
-		end
+		return on_response_or_references(error, result[1], client, on_response)
 	end
 
-	if not (client.name == "tsserver" or client.name == "vtsls" or client.name == "ts_ls") then
-		on_response(error, result, client)
-		return
+	if (not vim.islist(result)) or #result == 1 then
+		return on_response(error, result, client, on_response)
 	end
 
-	if vim.islist(result) and #result > 1 then
+	if is_typescript_client(client) then
 		local filtered = filter(result, filterReactDTS)
 		if #filtered <= 1 then
-			on_response(error, result, client)
-			return
+			return on_response_or_references(error, filtered[1], client, on_response)
 		end
-
-		local sameLine = everyOneIsSameLine(result)
-		if sameLine ~= nil then
-			on_response(error, result, client)
-			return
-		end
-
-		require("telescope.builtin").lsp_definitions()
-		return
 	end
 
-	return on_response(error, result, client)
+	if everyOneIsSameLine(result) then
+		return on_response_or_references(error, result[1], client, on_response)
+	end
+
+	require("telescope.builtin").lsp_definitions()
 end
 
 ---@param method string
