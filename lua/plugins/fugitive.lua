b0f7772649b9e9763d270cb796cbc47796b40a45
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-04-19 22:30:34 +0300

    hello

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,7 +6,10 @@ return {
 			pattern = "fugitive",
 			callback = function()
 				vim.keymap.set("n", "S", ":Git add .<CR>", { buffer = true, silent = true })
-				vim.keymap.set("n", "P", ":Git push<CR>:q<CR>", { buffer = true, silent = true })
+				vim.keymap.set("n", "P", function()
+					vim.api.nvim_command(":Git push<CR>")
+					vim.api.nvim_command(":q<CR>")
+				end, { buffer = true, silent = true })
 
 				vim.api.nvim_command("normal! 5G")
 			end,
