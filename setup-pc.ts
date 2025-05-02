daf0b3500773402987a17a66afcc295061c9fd54
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-05-02 10:27:30 +0300

    karabiner

--- setup-pc.ts
+++ setup-pc.ts
@@ -3,6 +3,7 @@ import { link, ensureLinesInFile } from "./ts/setup-pc-utils";
 link("~/.config/nvim/starsip.toml", "~/.config/starship.toml");
 link("~/.config/nvim/alacritty.toml", "~/.config/alacritty/alacritty.toml");
 link("~/.config/nvim/ideavimrc", "~/.ideavimrc");
+link("~/.config/nvim/karabiner.json", "~/.config/karabiner/karabiner.json");
 
 ensureLinesInFile({
   lines: ["source ~/.config/nvim/helpers.zshrc"],
